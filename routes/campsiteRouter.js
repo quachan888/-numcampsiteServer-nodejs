88fd397d566110fe75f92a27d003768ed2be9091
Andy Quach <quachan888@gmail.com>
2022-04-13 20:40:56 -0400

    Basic Authentication

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -5,41 +5,41 @@ const campsiteRouter = express.Router();
 
 campsiteRouter
     .route('/')
-    .get((req, res, next) => {
+    .get((req, res, next) =>
         Campsite.find()
             .then((campsite) => res.status(200).json(campsite))
-            .catch((err) => next(err));
-    })
-    .post((req, res, next) => {
+            .catch((err) => next(err))
+    )
+    .post((req, res, next) =>
         Campsite.create(req.body)
             .then((campsite) => res.status(200).json(campsite))
-            .catch((err) => next(err));
-    })
+            .catch((err) => next(err))
+    )
     .put((req, res, next) => res.status(403).end(`PUT operation not supported on /campsites`))
-    .delete((req, res, next) => {
+    .delete((req, res, next) =>
         Campsite.deleteMany()
             .then((response) => res.status(200).json(response))
-            .catch((err) => next(err));
-    });
+            .catch((err) => next(err))
+    );
 
 campsiteRouter
     .route('/:campsiteId')
-    .get((req, res, next) => {
+    .get((req, res, next) =>
         Campsite.findById(req.params.campsiteId)
             .then((campsite) => res.status(200).json(campsite))
-            .catch((err) => next(err));
-    })
+            .catch((err) => next(err))
+    )
     .post((req, res) => res.status(403).end(`POST operation not supported on /campsites/${req.params.campsiteId}`))
-    .put((req, res, next) => {
+    .put((req, res, next) =>
         Campsite.findByIdAndUpdate(req.params.campsiteId, { $set: req.body }, { new: true })
             .then((campsite) => res.status(200).json(campsite))
-            .catch((err) => next(err));
-    })
-    .delete((req, res, next) => {
+            .catch((err) => next(err))
+    )
+    .delete((req, res, next) =>
         Campsite.findByIdAndDelete(req.params.campsiteId)
             .then((response) => res.status(200).json(response))
-            .catch((err) => next(err));
-    });
+            .catch((err) => next(err))
+    );
 
 campsiteRouter
     .route('/:campsiteId/comments')
